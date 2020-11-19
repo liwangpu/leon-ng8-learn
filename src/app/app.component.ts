633c00f11c06610a83167a73b769944eaefa8e87
Leon Pu <puliwang@cxist.com>
2020-11-19 18:04:07 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+    public user = { name: 'robot' };
+    public changeName(): void {
+        this.user.name = Date.now().toString();
+    }
 }
