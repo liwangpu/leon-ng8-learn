37c9dca18be63287ad638bd34a2326c89a6b6e0f
Leon Pu <puliwang@cxist.com>
2020-12-10 11:22:05 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    public constructor(
+        private cd: ChangeDetectorRef
+    ) {
+
+    }
+
+    public ngOnInit(): void {
+        console.log(1, this.cd);
+    }
+
 
 }
